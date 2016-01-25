1308fb9e48c06a75416f765a97aa65aea180c608
Philip Ratcliffe <phil@philratcliffe.co.uk>
2016-01-25 18:03:25 +0000

    Messy update to work with just ISBN-10

--- extract.py
+++ extract.py
@@ -1,23 +1,67 @@
+import sys
+import operator
 import re
 from urlparse import urlparse
+import csv
+import pprint
 
 # Pattern by Gruber for extracting URLs from a text string
-
 GRUBER_URLINTEXT_PAT = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
+GRUBER_URLINTEXT_PAT = re.compile(r'https?://')
+ISBN_10_PAT = re.compile(r'dp/([0-9]{9}[\d|X])')
+#url.match("/([a-zA-Z0-9]{10})(?:[/?]|$)")
 
-ISBN_10_REGEX = r'/([0-9]{9}[\d|X])' # Amazon seems to be using these in the URL of books
+# ISBN-10 pattern. Amazon seems to be using these in URLs for books.
+ISBN_10_REGEX = r'/([0-9]{9}[\d|X])'
 
-with open('test_data', 'r') as f:
-    text = f.read()
-    urls = [ mgroups[0] for mgroups in GRUBER_URLINTEXT_PAT.findall(text) ]
+# File with Amazon URLs
+TEXT_FILENAME = 'HNCommentsAll.json'
 
-for url in urls:
-    parsed_url = urlparse(url)
-    if parsed_url and parsed_url.hostname:
-        if 'amazon.' in parsed_url.hostname:
-            m = re.search(ISBN_10_REGEX, url)
-            if m:
-                isbn = m.group(1)
-                print isbn
-            print url[:80]
+def save_dict(d):
+    w = csv.writer(open("output.csv", "w"))
+    for key, val in iter(sorted(d.items(), key=operator.itemgetter(1), reverse=True)):
+        #for key, val in d.items():
+        w.writerow([key, val])
+
+def read_file_in_chunks(file_object, chunk_size=4096):
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
+def process_urls(isbns, d):
+    for isbn in isbns:
+        if isbn == "0321968972":
+            print "another\n"
+        d[isbn] = d.get(isbn, 0) + 1
+
+def process_urls2(urls, d):
+    for url in urls:
+        if 'amazon' in url:
+            parsed_url = urlparse(url)
+            if parsed_url and parsed_url.hostname:
+                if 'amazon.' in parsed_url.hostname:
+                    m = re.search(ISBN_10_REGEX, url)
+                    if m:
+                        isbn = m.group(1)
+                        d[isbn] = d.get(isbn, 0) + 1
+
+    #with open(TEXT_FILENAME, 'r') as f:
+    #    text = f.read()
+    #    urls = [ mgroups[0] for mgroups in GRUBER_URLINTEXT_PAT.findall(text) ]
+
+d = {}
+f = open(TEXT_FILENAME, 'r')
+
+for i, chunk in enumerate(read_file_in_chunks(f)):
+    urls = ISBN_10_PAT.findall(chunk)
+    process_urls(urls, d)
+    save_dict(d)
+    if i % 10000 == 0:
+        print "========================================================"
+        print i
+        print(d)
+
 
