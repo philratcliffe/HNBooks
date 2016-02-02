55832742cab97c6fc2288df12aab1cc11cf3bd48
Philip Ratcliffe <phil@philratcliffe.co.uk>
2016-02-02 15:03:12 +0000

    Added config file to hold credentials to access Amazon API

--- extract.py
+++ extract.py
@@ -1,27 +1,40 @@
 import operator
 import re
-from urlparse import urlparse
 import csv
 
-# Pattern by Gruber for extracting URLs from a text string
-GRUBER_URLINTEXT_PAT = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
-GRUBER_URLINTEXT_PAT = re.compile(r'https?://')
+import ConfigParser
+
+from amazon.api import AmazonAPI
+
 ISBN_10_PAT = re.compile(r'dp/([0-9]{9}[\d|X])')
 
 # ISBN-10 pattern. Amazon seems to be using these in URLs for books.
 ISBN_10_REGEX = r'/([0-9]{9}[\d|X])'
 
-# File with Amazon URLs
-TEXT_FILENAME = 'HNCommentsAll.json'
+# File containing downloaded HN data (postings and comments)
+HN_DATA_FILENAME = 'HNCommentsAll.json'
+
+# A file containing Amazon credentials in the following format
+# [Credentials]
+# AMAZON_ACCESS_KEY:
+# AMAZON_SECRET_KEY:
+# AMAZON_ASSOC_TAG:
+
+AMAZON_CREDENTIALS_FILE = '.amazon_credentials'
+
 
 def save_dict(d):
+    """Write the dictionary in CSV format to a file"""
     w = csv.writer(open("output.csv", "w"))
 
-    # Iterate through the dictionary's key val pairs sorted by value (largest first)
+    # Iterate through the dictionary's key value pairs sorted by value
+    # (largest first)
     for key, val in iter(sorted(d.items(), key=operator.itemgetter(1), reverse=True)):
         w.writerow([key, val])
 
+
 def read_file_in_chunks(file_object, chunk_size=4096):
+    """Generator to read the potentially huge data file"""
     while True:
         data = file_object.read(chunk_size)
         if not data:
@@ -29,37 +42,41 @@ def read_file_in_chunks(file_object, chunk_size=4096):
         yield data
 
 
-def process_urls(isbns, d):
+def process_isbns(isbns, d):
+    """Count up the occurrences of each ISBN"""
     for isbn in isbns:
-        if isbn == "0321968972":
-            print "another\n"
         d[isbn] = d.get(isbn, 0) + 1
 
-def process_urls2(urls, d):
-    for url in urls:
-        if 'amazon' in url:
-            parsed_url = urlparse(url)
-            if parsed_url and parsed_url.hostname:
-                if 'amazon.' in parsed_url.hostname:
-                    m = re.search(ISBN_10_REGEX, url)
-                    if m:
-                        isbn = m.group(1)
-                        d[isbn] = d.get(isbn, 0) + 1
 
-    #with open(TEXT_FILENAME, 'r') as f:
-    #    text = f.read()
-    #    urls = [ mgroups[0] for mgroups in GRUBER_URLINTEXT_PAT.findall(text) ]
+def ConfigSectionMap(section):
+    """Helper function for accessing the config file"""
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+config = ConfigParser.ConfigParser()
+print config.read(AMAZON_CREDENTIALS_FILE)
+print config.sections()
+print ConfigSectionMap("Credentials")["amazon_access_key"]
+
 
 d = {}
-f = open(TEXT_FILENAME, 'r')
 
+# Open the file containing all the HN data (entries, comments etc)
+f = open(HN_DATA_FILENAME, 'r')
+
+# Look for an ISBN in URLs and keep track of those found
 for i, chunk in enumerate(read_file_in_chunks(f)):
-    urls = ISBN_10_PAT.findall(chunk)
-    process_urls(urls, d)
+    isbns = ISBN_10_PAT.findall(chunk)
+    process_isbns(isbns, d)
     save_dict(d)
     if i % 10000 == 0:
         print "========================================================"
         print i
         print(d)
-
-
